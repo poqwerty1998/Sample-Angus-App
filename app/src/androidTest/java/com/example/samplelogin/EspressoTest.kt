5fad41910968a7db00151cfcff4ff9042b32e2df
peter <peterxiao1998@gmail.com>
2019-07-02 10:12:07 -0400

    added 2 test cases to test the UI implemented so far, added ids to some of the xml layout files for the activities and changed button ids so they're unique.

--- app/src/androidTest/java/com/example/samplelogin/EspressoTest.kt
+++ app/src/androidTest/java/com/example/samplelogin/EspressoTest.kt
@@ -1,18 +1,14 @@
 package com.example.samplelogin
 
-import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.ViewAssertion
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.intent.matcher.IntentMatchers
-import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
+import androidx.test.espresso.matcher.RootMatchers
 import com.example.samplelogin.ui.MainActivity
-import com.example.samplelogin.ui.mainmenu.MainMenuActivity
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -33,7 +29,20 @@ class HelloWorldEspressoTest {
             .perform((typeText("admin")), closeSoftKeyboard())
         onView(withId(R.id.loginButton))
             .perform(click())
-        // checking the views in the activity
+        // if one of the buttons in the activity is found, login is successful
+        onView(withId(R.id.activity_main_menu))
+            .check(matches(withChild(withId(R.id.my_work_button))))
+    }
+    @Test
+    fun mainMenuTest() {
+        // typing in credentials to log in
+        onView(withId(R.id.username))
+            .perform((typeText("admin")), closeSoftKeyboard())
+        onView(withId(R.id.password))
+            .perform((typeText("admin")), closeSoftKeyboard())
+        onView(withId(R.id.loginButton))
+            .perform(click())
+        // if one of the buttons in the activity is found, login is successful
         onView(withId(R.id.activity_main_menu))
             .check(matches(withChild(withId(R.id.my_work_button))))
         onView(withId(R.id.activity_main_menu))
@@ -48,4 +57,65 @@ class HelloWorldEspressoTest {
         onView(withId(R.id.activity_main))
             .check(matches(withChild(withId(R.id.loginButton))))
     }
+    @Test
+    fun fourModulesTest() {
+        // typing in credentials to log in
+        onView(withId(R.id.username))
+            .perform((typeText("admin")), closeSoftKeyboard())
+        onView(withId(R.id.password))
+            .perform((typeText("admin")), closeSoftKeyboard())
+        onView(withId(R.id.loginButton))
+            .perform(click())
+        /*// my work module
+        onView(withId(R.id.my_work_button))
+            .perform(click())
+        onView(withId(R.id.activity_my_work))
+            .check(matches(withChild(withId(R.id.bottom_nav))))
+        onView(withId(R.id.bottom_nav))
+            .check(matches(withChild(withId(R.id.tenantRequestFragment))))
+        onView(withId(R.id.bottom_nav))
+            .check(matches(withChild(withId(R.id.preventiveMaintenanceFragment))))
+        onView(withId(R.id.bottom_nav))
+            .check(matches(withChild(withId(R.id.inspectionsFragment))))
+        onView(withId(R.id.activity_my_work))
+            .perform(pressBack())*/
+        // overview module
+        onView(withId(R.id.overview_button))
+            .perform(click())
+        onView(withId(R.id.activity_overview))
+            .check(matches(withChild(withId(R.id.overview_tr_button))))
+        onView(withId(R.id.activity_overview))
+            .check(matches(withChild(withId(R.id.overview_emergency_button))))
+        onView(withId(R.id.activity_overview))
+            .perform(pressBack())
+        // create module
+        onView(withId(R.id.create_button))
+            .perform(click())
+        onView(withId(R.id.activity_create))
+            .check(matches(withChild(withId(R.id.create_work_order_button))))
+        onView(withId(R.id.activity_create))
+            .check(matches(withChild(withId(R.id.create_notifications_button))))
+        onView(withId(R.id.activity_create))
+            .perform(pressBack())
+        // search module
+        onView(withId(R.id.search_button))
+            .perform(click())
+        onView(withId(R.id.activity_search))
+            .check(matches(withChild(withId(R.id.search_work_orders_button))))
+        onView(withId(R.id.activity_search))
+            .check(matches(withChild(withId(R.id.search_directory_button))))
+        onView(withId(R.id.activity_search))
+            .perform(pressBack())
+    }
+    /*@Test
+    fun loginFailure() {
+        // typing credentials that will not log the user in
+        onView(withId(R.id.username))
+            .perform((typeText("abc")), closeSoftKeyboard())
+        onView(withId(R.id.password))
+            .perform((typeText("abc")), closeSoftKeyboard())
+        onView(withId(R.id.loginButton))
+            .perform(click())
+        onView(withText("Username or password is incorrect.")).inRoot(ToastMatcher)
+    }*/             // need to create a custom toast matcher to check for toasts
 }
