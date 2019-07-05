c8091798c48979de96628e33df0341bd22e376aa
peter <peterxiao1998@gmail.com>
2019-07-05 16:51:45 -0400

    last commit before weekend, tidied up some code

--- app/src/main/java/com/example/samplelogin/ui/MainActivity.kt
+++ app/src/main/java/com/example/samplelogin/ui/MainActivity.kt
@@ -1,22 +1,18 @@
 package com.example.samplelogin.ui
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.samplelogin.R
 import com.example.samplelogin.data.LoginResponse
 import com.example.samplelogin.data.RetrofitClientInstance
 import com.example.samplelogin.ui.mainmenu.MainMenuActivity
-import kotlinx.android.synthetic.main.activity_main_menu.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-
-const val LOGIN_USERNAME = "admin"
-const val LOGIN_PASSWORD = "admin"
 
 class MainActivity : AppCompatActivity(){
 
@@ -28,38 +24,38 @@ class MainActivity : AppCompatActivity(){
     fun login(view: View) {
         val username = findViewById<EditText>(R.id.username).text.toString()
         val password = findViewById<EditText>(R.id.password).text.toString()
-        RetrofitClientInstance.retrofitInstance.validate(
-            "",
-            username,
-            password,
-            "Postman",
-            "2.23.0.48"
-        ).enqueue(object: Callback<LoginResponse>{
-            override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
-                //Toast.makeText(applicationContext, "Login success!", Toast.LENGTH_LONG).show()
-                if(response.isSuccessful) {
-                    val loginResponse = response.body()
-                    val mainMenuIntent = Intent(applicationContext, MainMenuActivity::class.java)
-                    mainMenuIntent.putExtra("loginResponse", loginResponse)
-                    startActivity(mainMenuIntent)
-                    Toast.makeText(applicationContext, "Login success!", Toast.LENGTH_LONG).show()
-                } else {
-                    /*when(response.code()) {
-                        302 -> {
-                            val mainMenuIntent = Intent(applicationContext, MainMenuActivity::class.java)
-                            startActivity(mainMenuIntent)
-                        }
-                        else -> {
-                            Toast.makeText(applicationContext, "Username or password is incorrect.", Toast.LENGTH_LONG).show()
-                        }
-                    }*/
+        val domain = ""
+        val userAgent = "Postman"
+        val version = "2.23.0.48"
+        if (username.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Username or password is empty.", Toast.LENGTH_LONG).show()
+        } else {
+            RetrofitClientInstance.retrofitInstance.validate(
+                domain,
+                username,
+                password,
+                userAgent,
+                version
+            ).enqueue(object : Callback<LoginResponse> {
+                override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
+                    if (response.isSuccessful) {
+                        val loginResponse = response.body()
+                        val mainMenuIntent = Intent(applicationContext, MainMenuActivity::class.java)
+                        mainMenuIntent.putExtra("loginResponse", loginResponse)
+                        startActivity(mainMenuIntent)
+                        Toast.makeText(applicationContext, "Login success!", Toast.LENGTH_LONG).show()
+                    } else {
+                        Toast.makeText(applicationContext,
+                            "Something wrong with the server. Please try again later.", Toast.LENGTH_LONG).show()
+                    }
                 }
-            }
-            override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
-                Toast.makeText(applicationContext, "Username or password is incorrect.", Toast.LENGTH_LONG).show()
-            }
+                override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                    Toast.makeText(applicationContext,
+                        "Something wrong with the server. Please try again later.", Toast.LENGTH_LONG).show()
+                }
 
-        })
+            })
+        }
 
     }
 }
